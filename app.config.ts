00f1514e050254eb29bd6bc4518716bd00c3fd82
5st7 <Not404friends@gmail.com>
2023-10-04 21:32:56 +0900

    wowow

--- app.config.ts
+++ app.config.ts
@@ -1,19 +1,19 @@
 export default defineAppConfig({
   alpine: {
-    title: 'GHOST IN THE SHELL',
+    title: '5st7 IN THE SHELL',
     description: 'たけるのブログです',
     image: {
-      src: '/social-card-preview.png',
-      alt: 'An image showcasing my project.',
+      src: '/ghost7.webp',
+      alt: 'たけるのブログです',
       width: 400,
       height: 300
     },
     header: {
       position: 'right', // possible value are : | 'left' | 'center' | 'right'
       logo: {
-        path: '/logo.svg', // path of the logo
-        pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
+        path: '/shell-logos_black.webp', // path of the logo
+        pathDark: '/shell-logos_white.webp', // path of the logo in dark mode, leave this empty if you want to use the same logo
+        alt: 'GHOST IN THE SHELL' // alt of the logo
       }
     },
     footer: {
@@ -21,7 +21,7 @@ export default defineAppConfig({
         enabled: false, // possible value are : true | false
         repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
       },
-      navigation: true, // possible value are : true | false
+      navigation: false, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
       message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
     },
